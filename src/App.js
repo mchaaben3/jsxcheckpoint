77fcc439a5a2617b98a71f1857884977422cf831
mchaaben3 <39343334+mchaaben3@users.noreply.github.com>
2021-08-16 17:29:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
 
                 <img src="/imageInPublic.jpg"  alt=''/>
 
+              
+          </div>
                 <video width="320" height="240" controls>
 
                      <source src="myVideo.mp4" type="video/mp4" />
 
                 </video>
-          </div>
-
     </div>
   );
 }
